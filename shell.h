cf15a453124e49292d15a3f115473168ddb52042
VusiBongani <sonofman012@gmail.com>
2023-08-22 23:27:10 -0700

    update

--- shell.h
+++ shell.h
@@ -12,21 +12,103 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
+
+/* for convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
+
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
 
 extern char **environ;
+
+
+/**
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - contains pseudo-arguements to pass.
+ * @arg: a generated string
+ * @argv:an array of strings
+ * @path: a string path.
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: copy of environ
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on with environ changed
+ * @status: the return status
+ * @cmd_buf: address of pointer to cmd_buf
+ * @cmd_buf_type: CMD_type
+ * @readfd: read line input from fd
+ * @histcount: the history line count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
+
+/**
+ * struct builtin - contains a string and function
+ * @type: command flag
+ * @func: function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
 
 int hsh(info_t *, char **);
 int find_builtin(info_t *);
@@ -127,76 +209,5 @@ int replace_alias(info_t *);
 int replace_vars(info_t *);
 int replace_string(char **, char *);
 
-/**
- * struct liststr - singly linked list
- * @num: representation of the number field
- * @str: a string variable
- * @next: points to the next node
- */
-typedef struct liststr
-{
-	int num;
-	char *str;
-	struct liststr *next;
-} list_t;
-
-/**
- *struct passinfo - contains pseudo-arguements to pass into a function.
- *@arg: a string generated from getline
- *@argv: an array of strings
- *@path: a string path
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit
- *@linecount_flag: count the line of input if on
- *@fname: the program filename
- *@env: linked list local
- *@environ: copy of environ from LL env
- *@history: the history node
- *@alias: the alias node
- *@env_changed: on if environment is changed
- *@status: Returns the status
- *@cmd_buf: address of pointer to cmd_buf
- *@cmd_buf_type: CMD_type
- *@readfd: reads the fd
- *@histcount: the line number count
- */
-typedef struct passinfo
-{
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	unsigned int line_count;
-	int err_num;
-	int linecount_flag;
-	char *fname;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environ;
-	int env_changed;
-	int status;
-
-	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-	int cmd_buf_type; /* CMD_type ||, &&, ; */
-	int readfd;
-	int histcount;
-} info_t;
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
-
-/**
- *struct builtin - has a built in string and the function
- *@type: represents the command flag
- *@func: representation of the function
- */
-typedef struct builtin
-{
-	char *type;
-	int (*func)(info_t *);
-} builtin_table;
-
 #endif
+
